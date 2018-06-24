78624218afd3ba9668598c1f28818dc95cbdd9c3
iwanttobealight <iwanttobealight@yandex.ru>
2018-06-24 22:43:13 +0300

    added TextDescription component

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 const propTypes = {
   onInputValueChange: PropTypes.func.isRequired,
   id: PropTypes.string,
-  value: PropTypes.string,
+  value: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.string), PropTypes.string]),
   type: PropTypes.string,
   className: PropTypes.string,
   name: PropTypes.string,
--- src/views/Text/Text.js
+++ src/views/Text/Text.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import nanoId from 'nanoid';
 
 import EditButton from '../../components/EditButton/EditButton';
 import TextTitle from '../../components/TextTitle/TextTitle';
 import TextSubtitle from '../../components/TextSubtitle/TextSubtitle';
+import TextDescription from '../../components/TextDescription/TextDescription';
 
 import './Text.scss';
 
@@ -24,6 +24,8 @@ class Text extends Component {
   state = {
     isEditing: false,
     title: this.props.data.title,
+    subtitle: this.props.data.subtitle,
+    description: this.props.data.description
   }
 
   componentStyles = {
@@ -44,14 +46,14 @@ class Text extends Component {
 
 
   render() {
-    console.log('title', this.state.title);
+    console.log('text state', this.state);
     return (
       <section className="text" style={this.componentStyles}>
         <div className="container">
           <div className="text__content" style={this.contentStyles}>
             <TextTitle title={this.state.title} isEditing={this.state.isEditing} handleChange={this.handleChange} />
             <TextSubtitle subtitle={this.state.subtitle} isEditing={this.state.isEditing} handleChange={this.handleChange} />
-            {this.props.data.description[0].replace(/\n/g, '***').split('***').map(item => <p key={nanoId()} className="text__description">{item}</p>)}
+            <TextDescription description={this.state.description} isEditing={this.state.isEditing} handleChange={this.handleChange} />
           </div>
           <EditButton onClick={this.toggleEditorState} isEditing={this.state.isEditing} />
         </div>
