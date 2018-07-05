5530d0ccba0d601e8f4b00276eebc8560fe3225d
iwanttobealight <iwanttobealight@yandex.ru>
2018-07-05 15:48:17 +0300

    deleted keyPress handler from withEditing

--- src/containers/HOC/withEditing.js
+++ src/containers/HOC/withEditing.js
@@ -1,24 +1,5 @@
 import React from 'react';
 
-const withEditing = (Component) => (
-  class ComponentWithEditing extends React.Component {
-
-    onEnterKeyPress = (e) => {
-      if (e.key === 'Enter' && e.shiftKey) {
-        document.execCommand('insertHTML', false, '<br/>');
-        e.preventDefault();
-      } else if (e.key === 'Enter') {
-        document.execCommand('insertHTML', false, '<br/><br/>');
-        e.preventDefault();
-      }
-    }
-
-    render() {
-      return (
-        <Component {...this.props} contentEditable suppressContentEditableWarning onBlur={() => console.log('blur')} onKeyPress={this.onEnterKeyPress} />
-      )
-    }
-  }
-)
+const withEditing = (Component) => (props) => <Component {...props} contentEditable suppressContentEditableWarning onBlur={() => console.log('blur')} />
 
 export default withEditing;
